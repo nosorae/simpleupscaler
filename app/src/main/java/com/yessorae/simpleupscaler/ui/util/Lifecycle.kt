a3e99ed9e2762dacd4dc01ad5dc9c6404afe6a9c
노소래 <yessorae@gmail.com>
2023-08-13 05:38:56 +0900

    feat: 컴포저블 라이프사이클 유틸 추가

---
+++ app/src/main/java/com/yessorae/simpleupscaler/ui/util/Lifecycle.kt
@@ -0,0 +1,24 @@
+package com.yessorae.simpleupscaler.ui.util
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
+
+@Composable
+fun ComposableLifecycle(
+    lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current,
+    onEvent: (LifecycleOwner, Lifecycle.Event) -> Unit
+) {
+    DisposableEffect(key1 = lifecycleOwner) {
+        val observer = LifecycleEventObserver { lifecycleOwner, event ->
+            onEvent(lifecycleOwner, event)
+        }
+        lifecycleOwner.lifecycle.addObserver(observer)
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+    }
+}
