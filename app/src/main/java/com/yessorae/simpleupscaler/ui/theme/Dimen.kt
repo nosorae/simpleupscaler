60c54bf7efeda5196deb357330a144ab5f53075a
노소래 <yessorae@gmail.com>
2023-08-12 16:27:50 +0900

    feat: theme, color, dimen 세팅

---
+++ app/src/main/java/com/yessorae/simpleupscaler/ui/theme/Dimen.kt
@@ -0,0 +1,13 @@
+package com.yessorae.simpleupscaler.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object Dimen {
+    val space_2 = 2.dp
+    val space_4 = 4.dp
+    val space_8 = 8.dp
+    val space_12 = 12.dp
+    val space_16 = 16.dp
+    val space_24 = 24.dp
+    val side_padding = 16.dp
+}
