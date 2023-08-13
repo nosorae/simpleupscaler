e717bb5863f1c01b9f0eec445dd2335829de8f54
노소래 <yessorae@gmail.com>
2023-08-13 21:04:16 +0900

    feat: 웹브라우저로 리다이렉트하는 함수 추가

--- app/src/main/java/com/yessorae/simpleupscaler/ui/util/Context.kt
+++ app/src/main/java/com/yessorae/simpleupscaler/ui/util/Context.kt
@@ -1,10 +1,14 @@
 package com.yessorae.simpleupscaler.ui.util
 
+import android.content.ActivityNotFoundException
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import com.yessorae.simpleupscaler.common.Logger
 import com.yessorae.simpleupscaler.ui.model.StringModel
 
 @Composable
@@ -12,4 +16,18 @@ fun getActivity() = LocalContext.current as ComponentActivity
 
 fun Context.showToast(stringModel: StringModel) {
     Toast.makeText(this, stringModel.get(this), Toast.LENGTH_LONG).show()
+}
+
+fun Context.redirectToWebBrowser(link: String, onActivityNotFoundException: () -> Unit) {
+    Logger.printLog("redirectToWebBrowser $link")
+    Intent(Intent.ACTION_VIEW, Uri.parse(link)).also { intent ->
+        try {
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            onActivityNotFoundException()
+            Logger.recordException(e)
+        } catch (e: Exception) {
+            Logger.recordException(e)
+        }
+    }
 }
