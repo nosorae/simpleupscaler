ebbfd18aadca022cacc9b60f3627ff899b5b28e1
노소래 <yessorae@gmail.com>
2023-08-13 21:24:59 +0900

    feat: 설정 앱 언어정보 가져오는 함수 추가

--- app/src/main/java/com/yessorae/simpleupscaler/ui/util/Context.kt
+++ app/src/main/java/com/yessorae/simpleupscaler/ui/util/Context.kt
@@ -4,12 +4,14 @@ import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.provider.Settings
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import com.yessorae.simpleupscaler.common.Logger
 import com.yessorae.simpleupscaler.ui.model.StringModel
+import java.util.Locale
 
 @Composable
 fun getActivity() = LocalContext.current as ComponentActivity
@@ -30,4 +32,10 @@ fun Context.redirectToWebBrowser(link: String, onActivityNotFoundException: () -
             Logger.recordException(e)
         }
     }
+}
+
+fun Context.getSettingsLocale(): Locale {
+    val config = android.content.res.Configuration()
+    Settings.System.getConfiguration(this.contentResolver, config)
+    return config.locales.get(0)
 }
