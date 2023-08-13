2bd6b6705705c8d3704a2d87778f2ce166c6f945
노소래 <yessorae@gmail.com>
2023-08-13 21:03:50 +0900

    feat: Logger클래스에 Crashytics, Analytics custom event 유틸 함수 추가

--- app/src/main/java/com/yessorae/simpleupscaler/common/Logger.kt
+++ app/src/main/java/com/yessorae/simpleupscaler/common/Logger.kt
@@ -1,6 +1,11 @@
 package com.yessorae.simpleupscaler.common
 
+import android.os.Bundle
 import android.util.Log
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.crashlytics.ktx.BuildConfig
+import com.google.firebase.crashlytics.ktx.crashlytics
+import com.google.firebase.ktx.Firebase
 
 object Logger {
     fun printLog(message: String) {
@@ -8,5 +13,42 @@ object Logger {
     }
     fun printError(message: String) {
         Log.e("SR-N", message)
+    }
+    fun recordException(e: Throwable) {
+        Firebase.crashlytics.recordException(e)
+    }
+
+    fun logAnalyticsEvent(
+        event: String,
+        vararg params: Pair<String, Any?>
+    ) {
+        if (BuildConfig.DEBUG) return
+
+        Bundle().apply {
+            params.forEach { pair ->
+                when (val value = pair.second) {
+                    is Int -> {
+                        putInt(pair.first, value)
+                    }
+
+                    is Boolean -> {
+                        putBoolean(pair.first, value)
+                    }
+
+                    else -> {
+                        (value as? String)?.let {
+                            putString(pair.first, value)
+                        } ?: run {
+                            putString(
+                                pair.first,
+                                value.toString()
+                            )
+                        }
+                    }
+                }
+            }
+        }.also { bundle ->
+            Firebase.analytics.logEvent(event, bundle)
+        }
     }
 }
