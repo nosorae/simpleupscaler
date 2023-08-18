996ca14d5740dfda43931545762262036723e62e
노소래 <yessorae@gmail.com>
2023-08-19 00:18:22 +0900

    fix: ktlint

--- app/src/main/java/com/yessorae/simpleupscaler/ui/components/UpscaleTopAppBar.kt
+++ app/src/main/java/com/yessorae/simpleupscaler/ui/components/UpscaleTopAppBar.kt
@@ -8,7 +8,6 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.yessorae.simpleupscaler.R
